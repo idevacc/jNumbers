c42a711babb86c77e400326865a47e2464ece7c4
jschuur <jschuur@jschuur.com>
2010-07-04 11:00:58 -0700

    Misc cleanup

--- Classes/CustomUIButton.m
+++ Classes/CustomUIButton.m
@@ -11,6 +11,8 @@
 
 @implementation CustomUIButton
 
+// Trick to add rounded corners to buttons, which you can't set in Interface Builder
+// via http://www.cimgf.com/2010/01/28/fun-with-uibuttons-and-core-animation-layers/
 - (id)initWithCoder:(NSCoder *)coder {
     if(self = [super initWithCoder:coder]) {
 		CALayer *layer = [self layer];
